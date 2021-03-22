183e4c73f713d133cef890812dd16724c5a1fa43
juker <juker1141@gmail.com>
2021-03-22 17:11:04 +0800

    update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'; // 命名reducer
 import authReducer from './authReducer';
 
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
