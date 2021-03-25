c63dd7ebd923fd03d34059a791480be92a7e7661
juker1141 <50107434+juker1141@users.noreply.github.com>
2021-03-26 00:30:56 +0800

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,8 +22,9 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
@@ -40,14 +41,14 @@ export const fetchStream = (id) => async (dispatch) => {
   dispatch({ type: FETCH_STREAM, payload: response.date });
 };
 
-export const deleteStream = (id) => async (dispatch) => {
-  await streams.delete(`/streams/${id}`);
-
-  dispatch({ type: DELETE_STREAM, payload: id });
-};
-
 export const editStream = (id, formValues) => async (dispatch) => {
   const response = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
